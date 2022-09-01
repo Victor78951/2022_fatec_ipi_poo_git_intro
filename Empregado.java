3993ffa03aaad4774fb0378a2e7d23edd29a95c5
Joao Victor <jv029543@gmail.com>
2022-09-01 16:19:12 -0300

    Commit inicial. Classe Empregado criada

---
+++ Empregado.java
@@ -0,0 +1,23 @@
+public class Empregado{
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
+
+/*  O encapsulador é o privete
+    Get e Set são metodos
+    comitir OBS: não pode falar cimitar
+    tracked & untracked
+*/
